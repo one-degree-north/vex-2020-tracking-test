eabf2f184a526c2dd2dd9de8d096a2c53761aa47
palarpalar <58934516+palarpalar@users.noreply.github.com>
2020-03-27 20:34:22 +0800

    Added basic exponetial drive control

--- include/subsystemHeaders/drive.hpp
+++ include/subsystemHeaders/drive.hpp
@@ -1,1 +1,11 @@
 #include "main.h"
+
+// HELPER FUNCTIONS
+void setDrive(int left, int right);
+double driveCurve(int input, double curve_factor);
+int round(int input);
+void printDrive();
+// AUTONOMOUS MOVEMENT FUNCTIONS
+void resetDriveMotors();
+// DRIVER CONTROL FUNCTIONS
+void setDriveMotors();
--- src/subsystem/drive.cpp
+++ src/subsystem/drive.cpp
@@ -1,1 +1,126 @@
 #include "main.h"
+
+// CONSTANTS
+const int DEADZONE = 5;
+const double CURVE_MAGNITUDE = 2.5;
+const int MAX_VALUE = 127;
+const double CURVE_FACTOR_FAST = 0.787;
+const double CURVE_FACTOR_SLOW = 0.4;
+
+// HELPER FUNCTIONS
+void setDrive(int left, int right){
+  /**
+   * Helper function to quickly set the velocities of left and right side motors
+   *
+   * @param left
+   *      Velocity of left side
+   * @param right
+   *      Velocity of right side
+   */
+  leftFront = left;
+  leftBack = left;
+  rightFront = right;
+  rightBack = right;
+}
+
+double driveCurve(int x, double curve_factor) {
+  /**
+   * Curves the velocity along an exponential curve
+   *
+   * @param x
+   *      Velocity to be curved
+   * @param curve_factor
+   *      Multiplier of entire curve function
+   * @return curved input if input is greater than the deadzone value or 0 if
+   * the value is less than the deadzone
+   */
+  if(abs(x) > DEADZONE){
+    int input = std::min((int)std::abs(x), MAX_VALUE);
+    double output = (x/abs(x)) * ((curve_factor) *
+                     (pow((input / pow(MAX_VALUE, (1 - (1 / CURVE_MAGNITUDE)))),
+                          CURVE_MAGNITUDE)));
+    return output;
+  }
+  else
+    return 0;
+}
+
+void printDrive(){
+  /**
+   * Outputs drive voltages to Brain screen using LCD emulator
+   *
+   */
+  int xVal = round(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
+  int yVal = round(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+
+  std::string left = "Left side: " +
+                      std::to_string(driveCurve(round(round(yVal)+round(xVal)),
+                      CURVE_FACTOR_FAST));
+  std::string right = "Right side: " +
+                      std::to_string(driveCurve(round(round(yVal)-round(xVal)),
+                      CURVE_FACTOR_FAST));
+
+  pros::lcd::set_text(2, left);
+  pros::lcd::set_text(3, right);
+}
+
+int round(int input){
+  /**
+   * Applies deadzone to joystick
+   *
+   * @param input
+   *      The value of an analog joystick
+   * @return input if input is greater than the deadzone value or 0 if the value
+   * is less than the deadzone
+   */
+  if(abs(input)>DEADZONE)
+    return input;
+  else
+    return 0;
+}
+
+
+// AUTONOMOUS FUNCTIONS
+void resetDriveMotors(){
+  /**
+   * Resets drivebase motor encoders to 0
+   *
+   */
+  leftFront.tare_position();
+  leftBack.tare_position();
+  rightFront.tare_position();
+  rightFront.tare_position();
+}
+
+// CHANGE TO OKAPI CHASSIS PID LATER
+void autoDrive(double dist, int voltage){
+  /**
+   * Autonomous function that drives the robot forward distance dist at speed voltage
+   * 
+   * @param dist
+   *      Distance to be travelled (in inches)
+   * @param voltage
+   *      Voltage drive motors to be set to
+   */
+  int dir = std::fabs(dist)/dist;
+  // calc avg displacement
+  double avg = leftFront.get_position() + leftBack.get_position() 
+             + rightFront.get_position() + rightBack.get_position();
+  while(std::fabs(avg) > std::fabs(dist)){
+    setDrive(dir*voltage, dir*voltage);
+    pros::delay(10);
+  }
+}
+
+// DRIVER CONTROL FUNCTIONS
+void setDriveMotors(){
+  /**
+   * Sets the drive motors using joystick values (arcade mode)
+   * 
+   */
+  int xVal = round(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
+  int yVal = round(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+
+  setDrive(driveCurve(round(round(yVal)+round(xVal)), CURVE_FACTOR_FAST),
+           driveCurve(round(round(yVal)-round(xVal)), CURVE_FACTOR_FAST));
+}
